bd3dafaf9d4411a73ab3a6c8be07c21e32ec32eb
Maheen Aqeel <Maheen.Aqeel@ad.jbs.live>
2023-04-20 12:09:12 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './Components/List';
+import AddToList from './Components/AddToList';
+
+export interface IState {
+  people: {
+      name: string
+      age: number
+      img: string
+      note?: string
+  }[]
+}
+
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Ali Ahmed",
+      age: 24,
+      img: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "Allegeric to staying on the same team",
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add to List</h1>
+      <List people={people}/>
+      <AddToList setPeople={setPeople} people={people}/>
     </div>
   );
 }
